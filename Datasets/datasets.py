b4be91e996ce63519e956a45ee66bd282c32cc08
Emmaculate Kamau <ekamau@637capital.com>
2024-02-17 11:31:51 +0300

    Preparing data for training with data loaders

--- Datasets/datasets.py
+++ Datasets/datasets.py
@@ -14,7 +14,7 @@ to enable easy access to the samples.
 """
 
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset, DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
@@ -66,4 +66,30 @@ def visualize_data():
         plt.imshow(img.squeeze(), cmap="gray")
     plt.show()
 
-visualize_data()
+
+"""
+The Dataset retrieves our dataset’s features and labels one sample at a time. 
+While training a model, we typically want to pass samples in “minibatches”, reshuffle the data at every epoch to reduce
+model overfitting, and use Python’s multiprocessing to speed up data retrieval.
+
+DataLoader is an iterable that abstracts this complexity for us in an easy API.
+"""
+
+
+def prepare_data_for_training():
+    training_data = training_data_()
+    test_data = test_data_()
+    train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
+    test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
+    train_features, train_labels = next(iter(train_dataloader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Labels batch shape: {train_labels.size()}")
+    img = train_features[0].squeeze()
+    label = train_labels[0]
+    plt.imshow(img, cmap="gray")
+    plt.show()
+    print(f"Label: {label}")
+
+
+prepare_data_for_training()
