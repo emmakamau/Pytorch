98ae5ca0281574bf69accc692ef757de94d24ea5
Emmaculate Kamau <ekamau@637capital.com>
2024-02-17 10:19:31 +0300

    Ignore unnecessary files

---
+++ Tensors/tensor.py
@@ -0,0 +1,5 @@
+/***
+Tensors are a specialized data structure that are very similar to arrays and matrices. In PyTorch, we use tensors to encode the inputs and outputs of a model, as well as the model’s parameters.
+
+import torch
+import numpy as np
